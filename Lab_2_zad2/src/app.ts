b061e78a7e0398ab8ede2b44b2dafb9f436b2ed4
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-04-11 10:49:47 +0200

    :game_die: game for two players

--- Lab_2_zad2/src/app.ts
+++ Lab_2_zad2/src/app.ts
@@ -1,8 +1,102 @@
+enum Sign {
+  "X",
+  "O",
+}
+class Cell {
+  cell: HTMLDivElement;
+  sign: Sign;
+  constructor(cell: HTMLDivElement) {
+    this.cell = cell;
+  }
+  setSign(sign: Sign) {
+    this.sign = sign;
+    switch (sign) {
+      case Sign.X:
+        this.cell.innerHTML = "X";
+        break;
+      case Sign.O:
+        this.cell.innerHTML = "O";
+        break;
+    }
+  }
+}
+class Board {
+  cells: Cell[] = [];
+  currentSign: Sign = Sign.X;
+  constructor() {
+    const cellsElements: NodeListOf<HTMLDivElement> = document.querySelectorAll(
+      ".cell"
+    );
+    cellsElements.forEach((cellElement: HTMLDivElement) => {
+      const cell = new Cell(cellElement);
+      this.cells.push(cell);
+      cellElement.addEventListener("click", () => {
+        this.makeChoice(cell);
+      });
+    });
+  }
+  makeChoice(cell: Cell) {
+    if (cell.sign !== null) return;
+    switch (this.currentSign) {
+      case Sign.X:
+        cell.setSign(this.currentSign);
+        this.currentSign = Sign.X;
+        break;
+      case Sign.O:
+        cell.setSign(this.currentSign);
+        this.currentSign = Sign.O;
+        break;
+    }
+    this.checkWin();
+  }
+  checkWin() {
+    for (let i = 0; i < 9; i += 3) {
+      if (this.cells[i].sign !== null)
+        if (
+          this.cells[i].sign === this.cells[i + 1].sign &&
+          this.cells[i + 1].sign === this.cells[i + 2].sign
+        )
+          this.gameOver();
+    }
+    for (let i = 0; i < 3; i++) {
+      if (this.cells[i].sign !== null)
+        if (
+          this.cells[i].sign === this.cells[i + 3].sign &&
+          this.cells[i + 3].sign === this.cells[i + 6].sign
+        )
+          this.gameOver();
+    }
+    if (this.cells[0].sign !== null)
+      if (
+        this.cells[0].sign === this.cells[4].sign &&
+        this.cells[4].sign === this.cells[8].sign
+      )
+        this.gameOver();
+    if (this.cells[2].sign !== null)
+      if (
+        this.cells[2].sign === this.cells[4].sign &&
+        this.cells[4].sign === this.cells[6].sign
+      )
+        this.gameOver();
+  }
+  gameOver() {
+    const gameOverElement = document.querySelector(".game-over-box");
+    if (gameOverElement) {
+      switch (this.currentSign) {
+        case Sign.X:
+          gameOverElement.innerHTML = "<h1>O wins!</h2>";
+          break;
+        case Sign.O:
+          gameOverElement.innerHTML = "<h1>X wins!</h2>";
+          break;
+      }
+    }
+  }
+}
 
-class App{
-    constructor() {
-    
+class App {
+  constructor() {
+    new Board();
+  }
 }
-}
-const app = new App();
-export default App;
+new App();
